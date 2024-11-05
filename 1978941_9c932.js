2bbcc501103a8b5067e144652c23c0e08f8c5bf8
eduardodzn <eduardokusumoto2016@gmail.com>
2024-11-05 11:18:31 -0300

    first commit

---
+++ 1978941_9c932.js
@@ -0,0 +1,14 @@
+window.mcwidget = {"appId":"532160876956612","pageId":"1978941","page_name":"BABI BRUM","fb_page_id":"","widget_phone":null,"sms_channel_connected":false,"widgets":[],"widgetLocale":"pt_BR","defaultSize":0,"fbSDKVersion":"v15.0","whitelist":["92f0c66c496628319b18d9a8e483fe822ba7a16cf6a3c09beb98cbafd8ff6365","e04a45f0a505569fddc714decd22a84e8cb421b628b371dd70f2ca7035eac346","36d89a1ba8cf52036f81a963c230938b18519267ee2a0e7203de79da8f03be17"],"smsModalConsentText":"Please note that by typing in your phone number in the field above you are agreeing to receive autodialed personal and marketing text messages to your mobile number. Consent is not required for purchase. Message and data rates may apply. Reply “STOP” by SMS to cancel."};
+
+
+(function(d, s, id){
+    var host = 'mccdn.me';
+
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) { return; }
+    js = d.createElement(s); js.id = id;
+    js.src = '//' + host + '/assets/js/widget.js';
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'mcwidget-core'));
+
+            
